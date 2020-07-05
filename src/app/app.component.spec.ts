6f9eb25fc168aa3d1eb84731d4d3dbcf01554925
Ryan Hutchison <ryan@gym.com>
2020-07-05 09:27:57 -0400

    chore: remove broken test (#17)
    https://github.com/ngneat/error-tailor/commit/940f13f071865b6d36cd06b24943a10fab5ba07c#diff-5c26d2c8f8838f32ace87fdecd6344eaL9
    (cherry picked from commit 87b54874fb6caac7a2d7b6c3f8c271a385e626d2)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -18,12 +14,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'error-tailor-playground'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('error-tailor-playground');
   });
 
   it('should render title', () => {
