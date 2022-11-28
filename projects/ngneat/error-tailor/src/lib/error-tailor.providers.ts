1ec93f0545e80d0edd27db47cd5892b784e04f63
Netanel Basal <netanel7799@gmail.com>
2022-11-28 08:05:56 +0200

    fix: 🐛 change imports array to module

--- projects/ngneat/error-tailor/src/lib/error-tailor.providers.ts
+++ projects/ngneat/error-tailor/src/lib/error-tailor.providers.ts
@@ -1,15 +1,22 @@
+import { NgModule } from '@angular/core';
 import { ControlErrorAnchorDirective } from './control-error-anchor.directive';
 import { DefaultControlErrorComponent } from './control-error.component';
 import { ControlErrorsDirective } from './control-error.directive';
 import { FormActionDirective } from './form-action.directive';
 import { ErrorTailorConfig, ErrorTailorConfigProvider, FORM_ERRORS } from './providers';
 
-export const errorTailorImports = [
+const _errorTailorImports = [
   ControlErrorsDirective,
   ControlErrorAnchorDirective,
   DefaultControlErrorComponent,
   FormActionDirective
 ];
+
+@NgModule({
+  imports: [_errorTailorImports],
+  exports: [_errorTailorImports]
+})
+export class errorTailorImports {}
 
 export function provideErrorTailorConfig(config: ErrorTailorConfig) {
   return [
